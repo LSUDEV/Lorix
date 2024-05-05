52d9103c02769b55a9ef1b442923055bed737af5
LSUDEV <105606754+LSUDEV@users.noreply.github.com>
2024-05-05 09:29:49 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -3,6 +3,7 @@ local HttpService = game:GetService("HttpService")
 -- <- Variables ->
 local Commits = HttpService:JSONDecode(game:HttpGet("https://api.github.com/repos/LSUDEV/Lorix/commits"))
 -- <- Main ->
+print('wokring')
 if Commits and typeof(Commits) == "table" and Commits[1] and Commits[1].sha then
     return loadstring(game:HttpGet("https://raw.githubusercontent.com/LSUDEV/Lorix/" .. Commits[1].sha .. "/Library.lua"))()
 end
