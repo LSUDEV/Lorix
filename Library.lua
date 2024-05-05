540c7d5753883b3f346692a916681d5e5eea096b
LSUDEV <105606754+LSUDEV@users.noreply.github.com>
2024-05-05 09:45:45 -0400

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1499,21 +1499,11 @@ do -- Library
                 end
                 --
                 do -- Functionss
-                    function Notification:Remove()
-                        --Notification.Objects["Outline"].Visible = false
-                    end
+  
                 end
                 --
                 do -- Connections
-                    Utility.General:Connect(RunService.RenderStepped, function()
-                        local Tick = tick()
-                        --
-                        if (((Tick - Notification.Tick) * 3000) >= Notification.Refresh) then
-                            Notification
-                            --
-                            Notification.Tick = Tick
-                        end
-                    end)
+
                 end
                 --
                 do -- Setup
