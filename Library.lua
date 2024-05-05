380cef79d8bddd6366387e082a866125a9552dcb
LSUDEV <105606754+LSUDEV@users.noreply.github.com>
2024-05-05 09:09:37 -0400

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1508,8 +1508,8 @@ do -- Library
                     Notification.Objects["Gradient_Line2"].Position = UDim2.new(0, 0, 0, 0)
                     --
                     print(self.Name);
-                    Notification.Objects["Text_Outline"].Text = self.Name
-                    Notification.Objects["Text"].Text = self.Name
+                    Notification.Objects["Text_Outline"].Text = string.format("[%s]: %s", os.date("%X"), self.Name)
+                    Notification.Objects["Text"].Text = string.format("[%s]: %s", os.date("%X"), self.Name)
                     Notification.Objects["Outline"].Size = UDim2.new(0, (Notification.Objects["Text_Outline"].TextBounds.X + 14), 0, 25)
                     --
                     Flags[Notification.Window]["Notification"] = Notification
