5d5d1e8b5fc8850735cb08ec7db6ec9f227cfa16
LSUDEV <105606754+LSUDEV@users.noreply.github.com>
2024-05-05 09:40:52 -0400

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1500,8 +1500,7 @@ do -- Library
                 --
                 do -- Functionss
                     function Notification:Remove()
-                        --
-                        Notification.Objects["Outline"].Visible = false
+                        --Notification.Objects["Outline"].Visible = false
                     end
                 end
                 --
